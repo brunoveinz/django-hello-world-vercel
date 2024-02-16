bbd3fbd993de65c854f12c58f0fbc4fe790312bd
bruno <bruno@condevuelta.cl>
2024-02-16 10:44:54 -0300

    bruno desde local

--- example/views.py
+++ example/views.py
@@ -8,7 +8,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Bruno desde local!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
